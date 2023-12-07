41f0a730aa6b97d7f8b88eab5eba4267836cb56a
szymqn <koodziejskisz@outlook.com>
2023-12-07 22:56:24 +0100

    Implemented custom user

--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import CustomUserChangeForm, LoginForm
 
-# Create your views here.
+
+def user_signup(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomUserChangeForm()
+    return render(request, 'users/signup.html', {'form': form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = LoginForm()
+    return render(request, 'users/login.html', {'form': form})
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
