880baf89da571deca1de42d5646c338c81b79d4b
szymqn <koodziejskisz@outlook.com>
2023-12-07 21:46:11 +0100

    Renamed main app

--- ProConstruct/wsgi.py
+++ ProConstruct/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ConstructFlow project.
+WSGI config for ProConstruct project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ConstructFlow.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProConstruct.settings')
 
 application = get_wsgi_application()
