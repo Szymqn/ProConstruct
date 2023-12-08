fa621c69bbf3593f9a9686aa84e9192f7ddbfdfd
szymqn <koodziejskisz@outlook.com>
2023-12-08 05:18:31 +0100

    Added js script

--- base/urls.py
+++ base/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<int:product_id>/', views.remove_from_cart, name='remove-from-cart'),
     path('cart/', views.view_cart, name='cart'),
+    path('increase-cart-item/<int:product_id>/', views.increase_cart_item, name='increase-cart-item'),
+    path('decrease-cart-item/<int:product_id>/', views.decrease_cart_item, name='decrease-cart-item'),
+    path('fetch-cart-count/', views.fetch_cart_count, name='fetch-cart-count'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Product, Cart, CartItem
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
 
 def index(request):
@@ -44,3 +45,48 @@ def view_cart(request):
     cart = request.user.cart
     cart_items = CartItem.objects.filter(cart=cart)
     return render(request, 'base/cart.html', {'cart_items': cart_items})
+
+
+@login_required(login_url='login')
+def increase_cart_item(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    cart = request.user.cart
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, product=product)
+
+    cart_item.quantity += 1
+    cart_item.save()
+
+    return redirect('cart')
+
+
+@login_required(login_url='login')
+def decrease_cart_item(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    cart = request.user.cart
+    cart_item = cart.cartitem_set.get(product=product)
+
+    if cart_item.quantity > 1:
+        cart_item.quantity -= 1
+        cart_item.save()
+    else:
+        cart_item.delete()
+
+    return redirect('cart')
+
+
+@login_required(login_url='login')
+def fetch_cart_count(request):
+    cart_count = 0
+    if request.user.is_authenticated:
+        cart = request.user.cart
+        cart_count = CartItem.objects.filter(cart=cart).count()
+    return JsonResponse({'cart_count': cart_count})
+
+
+def get_cart_count(request):
+    if request.user.is_authenticated:
+        cart_items = CartItem.objects.filter(cart=request.user.cart)
+        cart_count = cart_items.count()
+    else:
+        cart_count = 0
+    return cart_count
