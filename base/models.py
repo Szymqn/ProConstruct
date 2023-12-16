abd0f377dac7adedd6504ef197f0ea4dfcbe42b1
szymqn <koodziejskisz@outlook.com>
2023-12-16 15:24:38 +0100

    Created empty cart

--- base/models.py
+++ base/models.py
@@ -23,20 +23,15 @@ class Equipment(models.Model):
 
 
 class CartItem(models.Model):
-    cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
-    # product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    # equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', on_delete=models.CASCADE, null=True, blank=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
     equipment = models.ForeignKey(Equipment, on_delete=models.CASCADE, null=True, blank=True)
     product_quality = models.PositiveIntegerField(default=1)
     equipment_quality = models.PositiveIntegerField(default=1)
 
-    def __str__(self):
-        return f"{self.product_quality} x {self.product.name}, {self.equipment_quality} x {self.equipment.name}"
-
 
 class Cart(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
     products = models.ManyToManyField(Product, through='CartItem')
     equipments = models.ManyToManyField(Equipment, through='CartItem')
 
