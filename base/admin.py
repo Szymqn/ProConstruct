1ea493d9c55161306e9f7bb0471c0c72462bbcca
szymqn <koodziejskisz@outlook.com>
2023-12-16 02:08:14 +0100

    Added Equipment, implemented to cart

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Product, Cart, CartItem
+from .models import Product, Equipment, Cart, CartItem
 
 admin.site.register(Product)
+admin.site.register(Equipment)
 admin.site.register(CartItem)
 admin.site.register(Cart)
