cedaae53efe123ad9a8e35c5e8fee25c1e28eb94
szymqn <koodziejskisz@outlook.com>
2023-12-17 01:01:36 +0100

    Added reservaion fields

--- base/models.py
+++ base/models.py
@@ -19,6 +19,8 @@ class Equipment(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.URLField()
     quantity = models.PositiveIntegerField(default=1)
+    start_date = models.DateField()
+    end_date = models.DateField()
 
     def __str__(self):
         return self.name
